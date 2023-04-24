c638eba8e53c22aa39873af7aea38003cb7eb3e2
박선영 <sunny.playgnd@gmail.com>
2023-04-24 11:01:25 +0900

    test

--- 20230519/main.py
+++ 20230519/main.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.datasets import mnist 
+from tensorflow.keras.datasets import mnist   
 
 # the data, split between train and validation sets
 (x_train, y_train), (x_valid, y_valid) = mnist.load_data()
