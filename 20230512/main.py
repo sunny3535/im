517004db74f335ecd4ef5475c765f4aed3f2711b
박선영 <sunny.playgnd@gmail.com>
2023-05-02 12:23:05 +0900

    8주차 수업준비

--- 20230512/main.py
+++ 20230512/main.py
@@ -3,8 +3,8 @@ import numpy as np
 
 proto = 'models/colorization_deploy_v2.prototxt'
 weights = 'models/colorization_release_v2.caffemodel'
-   
 net = cv2.dnn.readNetFromCaffe(proto, weights)
+
 pts_in_hull = np.load('models/pts_in_hull.npy')
 pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1).astype(np.float32)
 net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull]
